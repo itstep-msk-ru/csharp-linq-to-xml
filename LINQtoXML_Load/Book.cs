53ab21fbfcf6e875064f2557605d957525142648
Jaroslav Wegner <jaroslav.wegner@gmail.com>
2021-04-08 14:07:14 +0300

    LINQ to XML

---
+++ LINQtoXML_Load/Book.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace LINQtoXML_Load {
+    class Book {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int? Year { get; set; }
+
+        public Book (string title, string author, int? year) {
+            Title = title;
+            Author = author;
+            Year = year;
+        }
+
+        public XElement ToXElement () => new XElement ("book",
+            new XElement ("title", this.Title),
+            new XElement ("author", this.Author),
+            new XElement ("year", this.Year));
+
+        public static Book FromXElement (XElement bookElement) => new Book (
+            (string) bookElement.Element ("title"),
+            (string) bookElement.Element ("author"),
+            (int?) bookElement.Element ("year"));
+
+        public override string ToString () => $"\"{Title}\", {Author}, {Year}";
+    }
+}
---
+++ LINQtoXML_Load/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace LINQtoXML_Load {
+    class Program {
+        static void Main (string[] args) {
+            // SaveBooks ();
+            LoadBooks ();
+        }
+
+        private static void LoadBooks () {
+            //string xml = File.ReadAllText ("books.xml");
+            //XElement doc = XElement.Parse (xml);
+
+            XElement root = XElement.Load ("books.xml");
+
+            //IEnumerable<XElement> bookElements = root.Elements ();
+            //IEnumerable<XElement> authors = bookElements.Elements ("author");
+            //foreach (XElement author in authors)
+            //    Console.WriteLine ((string) author);
+
+            //root.Elements ()
+            //    .Elements ("author")
+            //    .Select (authorElement => (string) authorElement)
+            //    .ToList ()
+            //    .ForEach (author => Console.WriteLine (author));
+
+            IReadOnlyList<Book> books = root.Elements ()
+                .Select (bookElement => Book.FromXElement (bookElement))
+                .ToList ();
+            foreach (Book book in books)
+                Console.WriteLine (book);
+        }
+
+        private static void SaveBooks () {
+            IReadOnlyList<Book> books = new[] {
+                new Book ("111", "Автор", 2019),
+                new Book ("222", "Автор", 2020),
+                new Book ("333", "Автор", 2019),
+                new Book ("444", "Автор", 2021),
+            };
+
+            XElement booksRoot = new XElement ("books", books.Select (book => book.ToXElement ()));
+            Console.WriteLine (booksRoot);
+            booksRoot.Save ("books-generated.xml");
+        }
+    }
+}
